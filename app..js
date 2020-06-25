82cbf34fc9e6400af7323e64ef2fed6fce08c7e7
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-25 21:40:44 +0500

    in app.js I add getNumber function

--- app..js
+++ app..js
@@ -1,1 +1,11 @@
-console.log("Calculator App");
+console.log("Calculator App by Hamza");
+
+
+function getNumber(num) {
+
+    console.log(num);
+    
+
+  var result = document.getElementById("result");  
+    
+}
