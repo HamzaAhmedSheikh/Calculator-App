6354087850d943b93fbfd7b6af040bed3a10c3d2
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-26 05:03:46 +0500

    calculator is completed

--- app..js
+++ app..js
@@ -1,6 +1,28 @@
 console.log("Calculator App by Hamza");
 
+var input = document.getElementById("input")
+var output = document.getElementById("output")
+
+
  function calculate(data) {
-   console.log('calc...', data);
+   
+    switch(data) {
+      case 'AC':
+        input.innerHTML = "";
+        output.innerHTML = 0;
+          
+          break;
+
+      case 'C':
+        input.innerHTML = input.innerHTML.substring(0, input.innerHTML.length - 1);    
+        output.innerHTML = eval(input.innerHTML) === undefined ? 0: eval(input.innerHTML);
+          break;
+
+      case '=': 
+        output.innerHTML = eval(input.innerHTML)   
+      default:
+        input.innerHTML += data   
+    }
+    
    
  }
