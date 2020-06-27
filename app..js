b8d0361f84b6ea9fe10524198f18bd94ea0d2fc5
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-27 23:06:35 +0500

    complete

--- app..js
+++ app..js
@@ -18,50 +18,11 @@ function back() {
   result.value = result.value.slice(0 , -1)
 }
 
-// // var input = document.getElementById("input")
-
-
-// function insert() {
-//   // var result = document.getElementById("result") 
-//   //   console.log(resut);
+function getResult(){
+  var result = document.getElementById("result")  
+  
+   result.value =  eval(result.value)
     
-//   //  result.value += num
+   
+}
 
-//    var myString = document.form.textview.value
-//    console.log(myString);
-//    var lastChar = myString[myString.length - 1]
-
-//     if(myString.length < 30) {
-//       if(!isNaN(lastChar) || lastChar == null || !isNaN(num)) {
-//         document.form.textview.value = myString + num;
-//       }
-//       else if(num != lastChar) {
-//         document.form.textview.value = myString.replace(lastChar, num)
-//       }
-//     }
-//   }  
-
-//   function equal() {
-//     var sum = document.form.textview.value
-
-//      if(sum) {
-//        document.form.textview.value = eval(sum)
-//      }
-//   }
-
-// function clearResult() {
-//   document.form.textview.value = ""
-// }  
-
-// // function getResult() {
-// //   var result = document.getElementById("result")
-
-// //    result.value = eval(result.value)
-
-
-// //  }
-
-// function back() {
-//   var exp = document.form.textview.value;
-//   document.form.textview.value = exp.substring(0, exp.length - 1)
-// }
