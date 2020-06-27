b43d5d94b4ceb0797d1487c6b52cf95c914617d8
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-27 20:55:42 +0500

    some changes but not working

--- app..js
+++ app..js
@@ -1,49 +1,51 @@
 console.log("Calculator App by Hamza");
 
-// var input = document.getElementById("input")
 
 
-function insert() {
-  // var result = document.getElementById("result") 
-  //   console.log(resut);
+// // var input = document.getElementById("input")
+
+
+// function insert() {
+//   // var result = document.getElementById("result") 
+//   //   console.log(resut);
     
-  //  result.value += num
+//   //  result.value += num
 
-   var myString = document.form.textview.value
-   console.log(myString);
-   var lastChar = myString[myString.length - 1]
+//    var myString = document.form.textview.value
+//    console.log(myString);
+//    var lastChar = myString[myString.length - 1]
 
-    if(myString.length < 30) {
-      if(!isNaN(lastChar) || lastChar == null || !isNaN(num)) {
-        document.form.textview.value = myString + num;
-      }
-      else if(num != lastChar) {
-        document.form.textview.value = myString.replace(lastChar, num)
-      }
-    }
-  }  
+//     if(myString.length < 30) {
+//       if(!isNaN(lastChar) || lastChar == null || !isNaN(num)) {
+//         document.form.textview.value = myString + num;
+//       }
+//       else if(num != lastChar) {
+//         document.form.textview.value = myString.replace(lastChar, num)
+//       }
+//     }
+//   }  
 
-  function equal() {
-    var sum = document.form.textview.value
+//   function equal() {
+//     var sum = document.form.textview.value
 
-     if(sum) {
-       document.form.textview.value = eval(sum)
-     }
-  }
+//      if(sum) {
+//        document.form.textview.value = eval(sum)
+//      }
+//   }
 
-function clearResult() {
-  document.form.textview.value = ""
-}  
+// function clearResult() {
+//   document.form.textview.value = ""
+// }  
 
-// function getResult() {
-//   var result = document.getElementById("result")
+// // function getResult() {
+// //   var result = document.getElementById("result")
 
-//    result.value = eval(result.value)
+// //    result.value = eval(result.value)
 
 
-//  }
+// //  }
 
-function back() {
-  var exp = document.form.textview.value;
-  document.form.textview.value = exp.substring(0, exp.length - 1)
-}
+// function back() {
+//   var exp = document.form.textview.value;
+//   document.form.textview.value = exp.substring(0, exp.length - 1)
+// }
